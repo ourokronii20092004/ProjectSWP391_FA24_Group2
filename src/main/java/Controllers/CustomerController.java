e21f953ed89befc2682e8aedd2f53b5abfb3c2b4
binhpp <phanphucbinh92@gmail.com>
2024-10-16 23:34:09 +0700

    add userprofile customer

---
+++ src/main/java/Controllers/CustomerController.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package Controllers;
+
+import DAOs.UserDAO;
+import Models.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Part;
+import java.io.File;
+
+/**
+ *
+ * @author phanp
+ */
+@WebServlet(name="CustomerController", urlPatterns={"/CustomerController"})
+public class CustomerController extends HttpServlet {
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    } 
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        HttpSession session = request.getSession();
+//        id = Integer.parseInt(session.getAttribute("user_id") + "");
+        //file upload
+        if (request.getParameter("uploadImg") != null) {
+
+            String fileName = "";
+
+            String uploadPath = "";
+            String s = getServletContext().getRealPath("") + File.separator;
+            uploadPath = getServletContext().getRealPath("") + File.separator;
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdirs();
+            }
+
+            for (Part part : request.getParts()) {
+                if (part.getName().equals("txtPic")) {
+//                    fileName = (String) getFileName(part);
+                    if (fileName != null && !fileName.isEmpty()) {
+                        part.write(uploadPath + File.separator +"img\\"+fileName);
+                    }
+                }
+            }
+            UserDAO dao = new UserDAO();
+//            User user = dao.getUserByID(id); //Fix file DAO
+//            dao.updateUserImg(id fileName);
+            response.sendRedirect("/UserController");
+
+        } else if (request.getParameter("btnSave") != null) {
+
+            String firstName = request.getParameter("txtCallName");
+            String lastName = request.getParameter("txtUserSurname");
+            String Email = request.getParameter("txtPhoneNumber");
+            String Address = request.getParameter("txtAddress");
+
+
+//            User obj = new User(id, firstName, lastName, Email, Address);
+            UserDAO dao = new UserDAO();
+//            int updated = dao.updateUserInfo(id, obj);
+//            if (updated == 0) {
+//                response.sendRedirect("/err" + id);
+//            } else {
+//                response.sendRedirect("/UserController");
+//            }
+
+        }
+    }
+}
--- src/main/java/DAOs/AccountDAO.java
+++ src/main/java/DAOs/AccountDAO.java
@@ -6,6 +6,7 @@ package DAOs;
 
 import DAOs.LoginDAO;
 import DB.DBConnection;
+import Models.User;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -34,21 +35,62 @@ public class AccountDAO {
         }
         return -1;
     }
+
+    //Binh
     
-//    public void updateProfile(int id, User newinfo) {
-//        DBConnection.Connect();
-//        if (DBConnection.isConnected()) {
-//            try {
-//                PreparedStatement pre = DBConnection.getPreparedStatement("UPDATE User"
-//                        + "SET Email = ?,"
-//                        + "    firstName = ?,"
-//                        + "    lastName=?,"
-//                        + "    address=?"
-//                        + "WHERE id = ?");
-//                pre.setString();
-//            } catch(Exception e){
-//                
-//            }
-//        }
-//    }
+    public void updateProfile(int id, User newinfo) {
+        DBConnection.Connect();
+
+        if (DBConnection.isConnected()) {
+
+            try ( PreparedStatement pre = DBConnection.getPreparedStatement(
+                    "UPDATE User "
+                    + "SET Email = ?, "
+                    + "    firstName = ?, "
+                    + "    lastName = ?, "
+                    + "    address = ? "
+                    + "WHERE id = ?")) {
+
+                pre.setString(1, newinfo.getEmail());
+                pre.setString(2, newinfo.getFirstName());
+                pre.setString(3, newinfo.getLastName());
+                pre.setString(4, newinfo.getAddress());
+
+                pre.executeUpdate();
+            } catch (SQLException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void viewProfile(int id, User user) {
+        DBConnection.Connect();
+
+        // Check if the connection is established
+        if (DBConnection.isConnected()) {
+            // Use try-with-resources to automatically close the PreparedStatement and ResultSet
+            try ( PreparedStatement pre = DBConnection.getPreparedStatement(
+                    "SELECT Email, firstName, lastName, address FROM User WHERE id = ?")) {
+
+                // Set the ID parameter for the query
+                pre.setInt(1, id);
+
+                // Execute the query and store the result in a ResultSet
+                try ( ResultSet rs = pre.executeQuery()) {
+                    // If the user with the given ID exists
+                    if (rs.next()) {
+                        // Populate the User object with data from the ResultSet
+                        user.setEmail(rs.getString("Email"));
+                        user.setFirstName(rs.getString("firstName"));
+                        user.setLastName(rs.getString("lastName"));
+                        user.setAddress(rs.getString("address"));
+                    }
+                }
+            } catch (SQLException e) {
+                // Handle any SQL exceptions (log or print)
+                e.printStackTrace();
+            }
+        }
+    }
 }
