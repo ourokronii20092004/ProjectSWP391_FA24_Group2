319369e6a0ddcb553ca51fbd927b3073b0e14ca8
LikeST27 <doremon27082004@gmail.com>
2024-10-16 10:40:45 +0700

    Add User Model Add EmployeeDAO with method : viewEmployeeList(), readEmployee(), addEmployee(), updateEmployee(), removeEmployee().

---
+++ src/main/java/DAO/EmployeeDAO.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import Model.User;
+import DB.DBConnection;
+import java.sql.*;
+import java.util.*;
+
+/**
+ *
+ * @author CE181515 - Phan Viet Phat
+ */
+public class EmployeeDAO {
+
+    ArrayList<User> empList = new ArrayList<>();
+
+    public EmployeeDAO() throws SQLException {
+        viewEmployeeList();
+    }
+
+    private void viewEmployeeList() throws SQLException {
+        empList.clear();
+        DBConnection.Connect();
+        if (DBConnection.isConnected()) {
+            ResultSet rs = DBConnection.ExecuteQuery("SELECT * from [dbo].[User] WHERE RoleID = 3");
+            while (rs.next()) {
+                empList.add(new User(rs.getInt("UserID"),
+                        rs.getString("Username"),
+                        rs.getString("Salt"),
+                        rs.getString("PasswordHash"),
+                        rs.getString("Email"),
+                        rs.getString("FirstName"),
+                        rs.getString("LastName"),
+                        rs.getString("ShippingAddress"),
+                        rs.getInt("RoleID"),
+                        rs.getByte("IsActive") == 1,
+                        rs.getDate("CreatedAt"),
+                        rs.getDate("UpdatedAt")));
+            }
+        }
+    }
+
+    public User readEmployee(int id) throws SQLException {
+        if (empList.isEmpty()) {
+            return null;
+        }
+        for (User u : empList) {
+            if (u.getId() == id) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    public void addEmployee(User emp) throws SQLException {
+        DBConnection.Connect();
+        if (DBConnection.isConnected()) {
+            PreparedStatement pre = DBConnection.getPreparedStatement("INSERT INTO [dbo].[User] VALUES(?,?,?,?,?,?,?,?,?,NULL,NULL)");
+            pre.setString(1, emp.getUserName());
+            pre.setString(2, emp.getSalt());
+            pre.setString(3, emp.getPassword());
+            pre.setString(4, emp.getEmail());
+            pre.setString(5, emp.getFirstName());
+            pre.setString(6, emp.getLastName());
+            pre.setString(7, emp.getAddress());
+            pre.setInt(8, emp.getRoleID());
+            pre.setInt(9, emp.isIsActive() ? 1 : 0);
+            pre.execute();
+            pre.close();
+            DBConnection.Disconnect();
+            viewEmployeeList();
+        }
+    }
+
+    public void updateEmployee(User emp) throws SQLException {
+        DBConnection.Connect();
+        if (DBConnection.isConnected()) {
+            PreparedStatement pre = DBConnection.getPreparedStatement("UPDATE [dbo].[User] SET "
+                    + "[FirstName] = ?"
+                    + ",[LastName] = ?"
+                    + ",[ShippingAddress] = ?"
+                    + ",[IsActive] = ?"
+                    + " WHERE UserID = ?");
+            pre.setString(1, emp.getFirstName());
+            pre.setString(2, emp.getLastName());
+            pre.setString(3, emp.getAddress());
+            pre.setInt(4, (emp.isIsActive() ? 1 : 0));
+            pre.setInt(5, emp.getId());
+            pre.execute();
+            pre.close();
+            DBConnection.Disconnect();
+            empList.set(empList.indexOf(readEmployee(emp.getId())), emp);
+        }
+    }
+
+    public boolean removeEmployee(int id) throws SQLException {
+        DBConnection.Connect();
+        if (DBConnection.isConnected()) {
+            int result = DBConnection.ExecuteUpdate("UPDATE [dbo].[User] "
+                    + "SET isActive = 0"
+                    + "WHERE UserID = " + id
+            );
+            DBConnection.Disconnect();
+            readEmployee(id).setIsActive(false);
+            return result > 0;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/Model/User.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author CE181515 - Phan Viet Phat
+ */
+public class User {
+    private final int id;
+    private String userName;
+    private String salt;
+    private String password;
+    private String Email;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int roleID;
+    private boolean isActive;
+    private final Date createdAt;
+    private final Date updatedAt;
+
+    public User(int id, String userName, String salt, String password, String Email, String firstName, String lastName, String address, int roleID, boolean isActive, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.userName = userName;
+        this.salt = salt;
+        this.password = password;
+        this.Email = Email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.roleID = roleID;
+        this.isActive = isActive;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String Salt) {
+        this.salt = Salt;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+}
