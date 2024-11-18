d03411466a41037a113853acc0428a1821f20fbb
LikeST27 <doremon27082004@gmail.com>
2024-11-18 16:30:44 +0700

    Fix finished

--- src/main/java/DAOs/VoucherDAO.java
+++ src/main/java/DAOs/VoucherDAO.java
@@ -25,35 +25,6 @@ import java.util.logging.Logger;
  */
 public class VoucherDAO {
 
-    //Phat
-    public Voucher getVoucherByOrderID(int voucherID) {
-        System.out.println("VoucherID = " + voucherID);
-        DBConnection.Connect();
-        if (DBConnection.isConnected()) {
-            try {
-            ResultSet rs = DBConnection.ExecuteQuery("SELECT * FROM [Vouchers] WHERE VoucherID = " + voucherID);
-                System.out.println("Voucher found!");
-                rs.next();
-                return new Voucher(
-                        rs.getInt("VoucherID"),
-                        rs.getString("VoucherCode"),
-                        (rs.getByte("DiscountType") == 1),
-                        rs.getFloat("DiscountValue"),
-                        rs.getDate("StartDate"),
-                        rs.getDate("EndDate"),
-                        rs.getString("VoucherName"),
-                        rs.getByte("isActive") == 1,
-                        rs.getDate("CreatedAt"),
-                        rs.getDate("UpdatedAt"));
-            } catch (SQLException ex) {
-                System.out.println("No voucher found!");
-                System.out.println(ex);
-                return null;
-            }
-        }
-        return null;
-    }
-
     private int upCount;
 
     public ArrayList<Voucher> getAllVouchers() {
